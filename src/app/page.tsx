3e6eb65a975213750fe4feab1f4902a18636ef01
Domogo <domsatrapa@gmail.com>
2024-08-06 15:53:19 +0200

    starter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,23 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <nav className="flex h-16 w-full justify-center border-b border-b-foreground/10">
-        <div className="flex w-full max-w-4xl items-center justify-between p-3 text-sm">
-          <AuthButton />
+      <div className="flex min-h-screen w-full flex-col items-center justify-between">
+        <nav className="flex h-16 w-full justify-center border-b border-b-foreground/10">
+          <div className="flex w-full max-w-4xl items-center justify-between p-3 text-sm">
+            <AuthButton />
+          </div>
+        </nav>
+
+        <div className="flex max-w-5xl flex-col items-center justify-center">
+          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+          </h1>
         </div>
-      </nav>
 
-      <div className="flex min-h-screen max-w-5xl flex-col items-center justify-center">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
+        <footer className="flex w-full justify-center border-t border-t-foreground/10 p-8 text-center text-xs">
+          <p>{hello.greeting}</p>
+        </footer>
       </div>
-
-      <footer className="flex w-full justify-center border-t border-t-foreground/10 p-8 text-center text-xs">
-        <p>{hello.greeting}</p>
-      </footer>
     </HydrateClient>
   );
 }
