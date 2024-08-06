ffa7e9916d8ff2f1fc439d53d4cec3ebe51c0706
Domogo <domsatrapa@gmail.com>
2024-08-06 15:13:02 +0200

    cleanup

--- src/app/_components/post.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export function LatestPost() {
-  return (
-    <div className="w-full max-w-xs">
-      <p className="truncate">Your most recent post: latest post</p>
-    </div>
-  );
-}
