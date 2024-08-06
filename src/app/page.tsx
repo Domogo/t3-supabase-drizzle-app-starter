97efef51858298d33c51e2e25c8ecb9f51591539
Domogo <domsatrapa@gmail.com>
2024-08-06 14:43:47 +0200

    shadcn, schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,6 @@ import { api, HydrateClient } from "~/trpc/server";
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
 
-  void api.post.getLatest.prefetch();
-
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
